dfe46e00bb7f456bc4522cb7deeff3d72566ffe9
Rafael Calunga <rafaelcalunga@gmail.com>
2020-09-03 23:58:00 -0400

    Disallow answers that are shorter than three letters or are just our start word

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -106,6 +106,11 @@ struct ContentView: View {
     }
     
     func isReal(word: String) -> Bool {
+        
+        if word.count < 3 || rootWord.starts(with: word) {
+            return false
+        }
+        
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
